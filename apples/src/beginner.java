79ad99cb70a51842bfa4b176cc6ad9bd22971b83
nickie <joanalicearmstrong@yahoo.com>
2013-04-27 21:09:22 -0700

    yes

---
+++ apples/src/beginner.java
@@ -0,0 +1,10 @@
+public class beginner {
+	public static void main (Strings args[]){
+		double tuna;
+		tuna = 5.28;
+		
+		System.out.println("i want");
+		System.out.print("tuna");
+		System.out.println("movie");
+	}
+}
